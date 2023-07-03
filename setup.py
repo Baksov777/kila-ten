4528d78e26565e98aed976f558d07318ed31659e
Baksov777 <138006410+Baksov777@users.noreply.github.com>
2023-07-03 04:54:15 +0200

    com

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='django-suit',
+    version=__import__('suit').VERSION,
+    description='Modern theme for Django admin interface.',
+    author='Kaspars Sprogis (darklow)',
+    author_email='info@djangosuit.com',
+    url='http://djangosuit.com',
+    packages=['suit', 'suit.templatetags'],
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'License :: Free for non-commercial use',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Environment :: Web Environment',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: User Interfaces',
+    ]
+)
